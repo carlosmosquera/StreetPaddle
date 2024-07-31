cf9f0a5f476e012099ac0526e0aab9e5fe7f2cef
Carlos Mosquera <kirkcemr@hotmail.com>
2024-07-31 08:52:07 -0700

    Initial Commit

---
+++ Street Paddle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Street Paddle
+//
+//  Created by Carlos Mosquera on 7/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Street Paddle/Street_PaddleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Street_PaddleApp.swift
+//  Street Paddle
+//
+//  Created by Carlos Mosquera on 7/31/24.
+//
+
+import SwiftUI
+
+@main
+struct Street_PaddleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
